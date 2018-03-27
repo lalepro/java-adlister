5fcec2acc1ed3450dff539760fdc9fae59f2a1d3
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-26 21:21:39 -0500

    attempted to refactor emma's code

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -9,27 +9,69 @@ import javax.servlet.http.*;
 
 public class CounterServlet extends HttpServlet {
     private int hitCount;
+    String reset;
 
-    public void init(){
+
+    public void init() {
         hitCount = 0;
     }
 
-@Override
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    response.getWriter().println("<h1>Hello, World!</h1>");
-
-    String restart = request.getParameter("restart");
-    if (restart == null){
         hitCount++;
-        response.getWriter().println("<h2> Visitors: " + hitCount + "</h2>");
-    } else {
-        hitCount = 0;
-        response.getWriter().println("<h2> Visitors: " + hitCount + "</h2>");
-    }
-    }
+        response.getWriter().println("<h1>Hello, World!</h1>");
+        response.getWriter().println("<h1>Counter!</h1>");
+        response.getWriter().println("<h2>Total Number of hits: " + hitCount + "</h2>");
+
+        if (reset != null && reset.equalsIgnoreCase("reset")) {
+
+            hitCount = 0;
+            reset = request.getParameter("reset");
+            response.getWriter().println("<h2> Visitors: " + hitCount + "</h2>");
+
+        }
+
+        response.getWriter().println("<form>");
+        response.getWriter().println("<input name = 'reset' value='Reset' type='submit'>");
+        response.getWriter().println("</form>");
+        String Count;
+        Count = request.getParameter(" ");
+        if (Count != null && Count.equalsIgnoreCase(" ")) {
+            hitCount = 0;
+            response.getWriter().println("<form>");
+            response.getWriter().println("<input name = 'Count' value='Count' type='submit'>");
+            response.getWriter().println("</form>");
+        }
+
+//        response.getWriter().println("<h1>Counter!</h1>");
+//        response.getWriter().println("<h2>Total Number of hits: " + hitCount + "</h2>");
+//        do {
+//            String reset = request.getParameter("reset");
+//            if (reset != null && reset.equalsIgnoreCase("reset")) {
+//                hitCount = 0;
+//            } else {
+//                hitCount++;
+//            }
+//            response.getWriter().println("<form>");
+//            response.getWriter().println("<input name = 'reset' value='Reset' type='submit'>");
+//            response.getWriter().println("</form>");
+//        } while (reset != null) {
+//            String Count = request.getParameter(" ");
+//            if (Count != null && Count.equalsIgnoreCase("count")) {
+//                hitCount = 0;
+//            } else {
+//                hitCount++;
+//            }
+//        }
+//        response.getWriter().println("<form>");
+//        response.getWriter().println("<input name = 'Count' value='Count' type='submit'>");
+//        response.getWriter().println("</form>");
+
 //    http://localhost:8080/counter?restart=restart
 
+    }
 }
+
 
 //        hitCount++;
 //    String name = request.getParameter("name");
