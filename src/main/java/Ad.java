870e0fcc272e8fa081a22d137da90fbdc5e1a575
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-04-03 18:02:32 -0500

    save changes

--- src/main/java/Ad.java
+++ src/main/java/Ad.java
@@ -17,6 +17,9 @@ public class Ad {
         this.description = description;
     }
 
+    public Ad(){
+
+    }
     public long getId() {
         return id;
     }
---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,7 @@
+import java.sql.Connection;
+
+public class MySQLAdsDao implements Ads {
+    private Connection connection = null;
+
+
+}
